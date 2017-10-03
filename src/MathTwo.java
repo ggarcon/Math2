fd0a13817f8e60299551e5ee8792e014612c67b9
Gaetan Garcon <gaetangarcon@Gaetans-MacBook-Air.local>
2017-10-02 20:26:34 -0400

    Initial commit

---
+++ src/MathTwo.java
@@ -0,0 +1,12 @@
+public class MathTwo {
+
+    public static void main(String[] args) {
+        double MyOtherNumber = 1.7938;
+        int MyNumber = 2;
+
+        System.out.println("My number is " +MyNumber );
+        System.out.println("My other number is " +MyOtherNumber);
+
+
+    }
+    }
